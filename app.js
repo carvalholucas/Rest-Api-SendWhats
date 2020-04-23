1569d9d1e5fbc36a3c67f4e79566f8605a3bd262
Lucas Carvalho <lucas.carvalho@azymus.com.br>
2020-04-23 00:33:16 -0300

    Correction of CORS

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use('/verify', routeVerify)
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 
     if (req.method === "OPTIONS") {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
         return res.status(200).send({})
     }
 
